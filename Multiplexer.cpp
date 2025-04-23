e352b9486a5cf37288dd2728c7058bb0d0c16c90
abbouram <abbouram@e1r1p1.1337.ma>
2025-04-23 02:41:06 +0100

    Bring Multiplexer.cpp from main branch

--- Multiplexer.cpp
+++ Multiplexer.cpp
@@ -6,9 +6,27 @@ bool Multiplexer::isServerSocket(int fd) {
 }
 
 int Multiplexer::create_server_socket(unsigned short currentPort, std::string host) {
-
+    
     //// I need first to check is already have a server with the same host , Port ///////
+    
+    static std::vector<std::pair<unsigned short, std::string> > binded;
+    static std::map<std::pair<unsigned short, std::string>, int> socketServer;
 
+
+    bool found = false;
+    std::pair<unsigned short, std::string> key_to_find = std::make_pair(currentPort, host);
+    for (std::vector<std::pair<unsigned short, std::string> >::iterator it = binded.begin(); it != binded.end(); ++it) {
+        if (it->first == key_to_find.first && it->second == key_to_find.second) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        return socketServer[key_to_find] * -1;
+    }
+    
+    
     int Socket = socket(AF_INET, SOCK_STREAM, 0);
     if (Socket == -1)
         throw std::runtime_error("socket failed !!!!!!!");
@@ -28,6 +46,10 @@ int Multiplexer::create_server_socket(unsigned short currentPort, std::string ho
 
     if (listen(Socket, SOMAXCONN) < 0)
         throw std::runtime_error("listen failed !!!!!!!!!!");
+    
+    binded.push_back(key_to_find);
+    socketServer[key_to_find] = Socket;
+
     return Socket;
 }
 
@@ -50,7 +72,11 @@ void Multiplexer::startMultiplexing(confugParser& config)
         for (size_t j = 0; j < portsCount; j++)
         {
             int socketFd = this->create_server_socket(Ports[j], host);
-
+            if (socketFd < 0) 
+            {
+                config.GetAllData()[i]->SetServerSocket(socketFd * -1);
+                continue;
+            }
             struct epoll_event event;
             
             event.events = EPOLLIN;
@@ -63,68 +89,85 @@ void Multiplexer::startMultiplexing(confugParser& config)
         }
     }
 
+
     ///////
-    this->run();
+    this->run(config);
 }
 
-void Multiplexer::NewClient(int eventFd)
+int Multiplexer::NewClient(int eventFd)
 {
+    
     int clientFd = accept(eventFd, NULL, NULL);
     if (clientFd == -1) {
         std::cerr << "accept failed" << std::endl;
-        return;
+        return -1;
     }
-    client[clientFd] = Client(clientFd, this->EpoleFd);
+
+    struct epoll_event event;
+    event.events = EPOLLIN | EPOLLOUT;
+    event.data.fd = clientFd;
+    epoll_ctl(this->EpoleFd, EPOLL_CTL_ADD, clientFd, &event);
+    return clientFd;
 }
 
-void Multiplexer::handelResponse(int eventFd) {
-    
+
+void Multiplexer::handelRequest(int eventFd, std::string buffer, size_t bytesReaded) {
+
+    // ///
+    (void)buffer;
+    (void)bytesReaded;
+    (void)eventFd;
+
+// ela saliti parsing {}
+        struct epoll_event event;
+        event.events = EPOLLOUT | EPOLLET;
+        event.data.fd = eventFd;
+
+        if (epoll_ctl(this->EpoleFd, EPOLL_CTL_MOD, eventFd, &event) == -1) {
+            std::cerr << "epoll_ctl failed to modify event" << std::endl;
+        }
+}
+
+
+void Multiplexer::handelResponse(int eventFd, confugParser &confug) {
+
     if (eventFd == -1) {
-        // std::cerr << "fd is invalid, connection closed?" << std::endl;
+        std::cerr << "fd is invalid, connection closed?" << std::endl;
         return;
     }
     
     // std::cout << "handelResponse of fd " << eventFd << std::endl;
     std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!";
 
-    ssize_t bytesSent = send(eventFd, response.c_str(), response.size(), 0);
+    ssize_t bytesSent = send(eventFd, response.c_str(), response.size(), MSG_NOSIGNAL);
 
     if (bytesSent == -1) {
-        // std::cerr << "send failed for fd: " << eventFd << std::endl;
+        std::cerr << "send failed for fd: " << eventFd << std::endl;
         close(eventFd);
+        epoll_ctl(this->EpoleFd, EPOLL_CTL_DEL, eventFd, NULL);
+        confug.removeClient(eventFd);
         return;
     }
+   
+    std::cerr << "send succesfull for fd: " << eventFd << std::endl;
     struct epoll_event event;
     event.events = EPOLLIN | EPOLLET;
     event.data.fd = eventFd;
 
     if (epoll_ctl(this->EpoleFd, EPOLL_CTL_MOD, eventFd, &event) == -1) {
-        // std::cerr << "epoll_ctl failed to modify event" << std::endl;
+        std::cerr << "epoll_ctl failed to modify event" << std::endl;
     }
-    
+
 }
 
-void    check_message(int eventFd){
-    const char *http_response = 
-    "HTTP/1.1 200 OK\r\n"
-    "Content-Type: text/html\r\n"
-        "Content-Length: 19\r\n"  // Length of the message below
-        "\r\n"
-        "waslatna chokrane";  // Body content
-        send(eventFd, http_response, strlen(http_response), 0);
-}
-
-void Multiplexer::handelRequest(int eventFd) {
-    (void) eventFd;
-}
-
-void Multiplexer::run() {
+void Multiplexer::run(confugParser& config) {
     
     while (true) {
+
         struct epoll_event events[1024];
         int eventCount = epoll_wait(this->EpoleFd, events, 1024, -1);
         if (eventCount == -1) {
-            // std::cerr << "epoll_wait failed" << std::endl;
+            std::cerr << "epoll_wait failed" << std::endl;
             continue ;
         }
         else if (eventCount == 0)
@@ -135,24 +178,51 @@ void Multiplexer::run() {
 
             // If it is a server socket we need to accept new client
             if (isServerSocket(eventFd)) {  
-                static int count = 0;
-                count++;
-                NewClient(eventFd);
-                std::cout << GREEN << "New connection accepted: " << COLOR_RESET << count  << std::endl;
+                int clientSocket = NewClient(eventFd);
+                if (clientSocket != -1)
+                {
+                    config.newClient(clientSocket, eventFd);
+                    size_t count = config.GetAllData().size();
+                    for (size_t i = 0; i < count; i++)
+                    {
+                        if (config.GetAllData()[i]->isTheSeverSocket(eventFd))
+                            clientOfServer[clientSocket] = config.GetAllData()[i];
+                    }
+                    
+                }
             }
             // Check if the event is for reading
             else if (events[i].events & EPOLLIN) {
-                //std::cout << YELLOW << "Read event on fd: " << COLOR_RESET << eventFd << std::endl;
-                if (client.find(eventFd) != client.end()) {
-                    client[eventFd].Request();
-                    check_message(eventFd);
-                    // handelRequest(eventFd);
+                char buffer[1024];
+                size_t bytesReaded = read(eventFd, buffer, BUFFERSIZE);
+
+                // if the client disconeect or other issue 
+                if (bytesReaded <= 0) {
+                    close(eventFd);
+                    epoll_ctl(this->EpoleFd, EPOLL_CTL_DEL, eventFd, NULL);
+                    config.removeClient(eventFd);
+                    clientOfServer.erase(eventFd);
+                }
+                else
+                {
+                    buffer[bytesReaded] = '\0';
+                    handelRequest(eventFd ,buffer, bytesReaded);
                 }
             }
             // Check if the event is for writting 
+            else if (events[i].events & EPOLLOUT) {
+                handelResponse(eventFd, config);
+                close(eventFd);
+                epoll_ctl(this->EpoleFd, EPOLL_CTL_DEL, eventFd, NULL);
+                config.removeClient(eventFd);
+                clientOfServer.erase(eventFd);
+            }
         }
     }
 }
+
+
+
 
 Multiplexer::Multiplexer(/* args */)
 {
